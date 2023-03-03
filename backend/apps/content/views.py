72c85fbf7c901acc82ea99e043c8720b14007477
anonimus007-700 <84788593+anonimus007-700@users.noreply.github.com>
2023-03-03 21:23:23 +0200

    not allowed method "GET" in /register/

--- backend/apps/content/views.py
+++ backend/apps/content/views.py
@@ -19,16 +19,5 @@ class ContentView(APIView):
     def post(self, request):
         serializer = ContentSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            # user = Content.objects.all()
-            # obg = serializer.validated_data
-            # has_user = False
-
-            # for i in user:
-            #     if (obg['login'] == i.login or
-            #          obg['email'] == i.email):
-            #         has_user = True
-            #         break
-
-            # if has_user == False:
             serializer.save()
             return Response({"complite": True})
